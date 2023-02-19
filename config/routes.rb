90abba5b8e5493c59b1625168c86410065c2c71f
Ernesto Argentina Cumbe <ernestoargentina@gmail.com>
2023-02-20 01:53:11 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "metrics#index"
+
+  # get metrics
+  get 'api/metrics'=>'metrics#getMetrics'
+
+  # get metrics timeline hour
+  get 'api/metrics/timeline'=>'metrics#timeline'
+
+  # get metrics timeline day
+  get 'api/metrics/timeline/day'=>'metrics#timelineDay'
+
+  # get metrics timeline minutes
+  get 'api/metrics/timeline/minute'=>'metrics#timelineMinute'
+
+  #Resources routes
+  resources :metrics
 end
